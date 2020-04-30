787d8494b0fcd94a2e0719c88b68257a1428cf81
Cristina-Andreea PAVEL <cristina.pavel1611@stud.acs.upb.ro>
2020-04-30 17:55:08 +0300

    Created project

---
+++ Proiect IAME/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Proiect_IAME.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Proiect_IAME.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Proiect_IAME.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Proiect IAME/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proiect_IAME.Startup))]
+namespace Proiect_IAME
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
